436fa27dcc44d46a65ab8f2e3cf1143a784ff281
abdulkader <abdulkader_hassan74@hotmail.com>
2021-08-25 23:24:00 +0200

    added extended type of request to be able to use user from Req

--- server/src/configs/types.d.ts
+++ server/src/configs/types.d.ts
@@ -1,5 +1,13 @@
+import express from "express";
 import { IncomingHttpHeaders } from "http";
+import User from "../models/User.model";
 
+interface Request extends express.Request {
+    user?:User
+}
+ 
+interface Response extends express.Response {
+}
 export interface LoginCredentials{
     name:string;
     password:string;
