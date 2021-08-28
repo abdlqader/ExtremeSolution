e35cc70109cdad6b0b84293471e9ff2f2bc04dee
abdulkader <abdulkader_hassan74@hotmail.com>
2021-08-28 13:46:44 +0200

    reducers are now fully connected with the backend

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -1,3 +1,16 @@
 import { FETCH_USERS } from '../services/types';
 
-export const fetchUsers = () => (dispatch) => {};
+export const fetchUsers = (token) => (dispatch) => {
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/art`, {
+      headers: { Authorization: `${token}` },
+    })
+    .then((response) => {
+      dispatch({
+        type: FETCH_USERS,
+        payload: {
+          users: response.data,
+        },
+      });
+    });
+};
