a96d879dc5cc0e83f16b7d4057ecd07b481206d5
abdulkader <abdulkader_hassan74@hotmail.com>
2021-08-28 14:13:58 +0200

    missed axios import

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -1,5 +1,5 @@
 import { FETCH_USERS } from '../services/types';
-
+import axios from 'axios';
 export const fetchUsers = (token) => (dispatch) => {
   axios
     .get(`${process.env.REACT_APP_API_URL}/art`, {
